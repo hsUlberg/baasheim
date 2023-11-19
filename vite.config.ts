9e47cbb1dbe6ece1b94ea9773bd431b9455eeaa7
hsUlberg <helge.sondre@gmail.com>
2023-11-19 16:23:36 +0100

    fix: update vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://hsulberg.github.io/baasheim/',
+  base: '/baasheim/',
   plugins: [reactRefresh()],
 });
