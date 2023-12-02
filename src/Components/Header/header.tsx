6e2048eb8c54a9fe6e1b97a353ec37f51e616463
hsUlberg <helge.sondre@gmail.com>
2023-12-02 22:59:11 +0100

    fix: fix header buttons

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { clsx } from 'clsx';
 
 export function Header() {
   const [active, setActive] = useState<string>(window.location.hash.substring(2));
@@ -9,51 +10,51 @@ export function Header() {
       <div className="font-[LoraRegular] text-4xl m-1 text-center">
         Båsheim Gjestegård
       </div>
-      <div className="border-t-2 border-b-2 text-center">
-        <button
-          className={
-            active == ''
-              ? 'bg-gray-300 cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-              : 'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-          }
+      <div className="border-t-2 border-b-2 text-center py-1">
+        <Link
           onClick={() => {
             setActive('');
-          }}>
-          <Link to="/">Hjem</Link>
-        </button>
-        <button
-          className={
-            active == 'overnatting'
-              ? 'bg-gray-300 cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-              : 'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-          }
+          }}
+          className={clsx([
+            'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded',
+            active == 'overnatting' && 'bg-gray-300',
+          ])}
+          to="/">
+          Hjem
+        </Link>
+        <Link
           onClick={() => {
             setActive('overnatting');
-          }}>
-          <Link to="/overnatting">Overnatting</Link>
-        </button>
-        <button
-          className={
-            active == 'selskapslokale'
-              ? 'bg-gray-300 cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-              : 'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-          }
+          }}
+          className={clsx([
+            'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded',
+            active == 'overnatting' && 'bg-gray-300',
+          ])}
+          to="/overnatting">
+          Overnatting
+        </Link>
+        <Link
           onClick={() => {
             setActive('selskapslokale');
-          }}>
-          <Link to="/selskapslokale">Selskapslokale</Link>
-        </button>
-        <button
-          className={
-            active == 'groset'
-              ? 'bg-gray-300 cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-              : 'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded'
-          }
+          }}
+          className={clsx([
+            'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded',
+            active == 'overnatting' && 'bg-gray-300',
+          ])}
+          to="/selskapslokale">
+          Selskapslokale
+        </Link>
+        <Link
           onClick={() => {
             setActive('groset');
-          }}>
-          <Link to="/groset">Grøset seter</Link>
-        </button>
+          }}
+          className={clsx([
+            'cursor-pointer text-xl px-2 py-1 hover:bg-gray-400 rounded',
+            active == 'overnatting' && 'bg-gray-300',
+          ])}
+          to="/groset">
+          Grøset seter
+        </Link>
       </div>
     </div>
   );
